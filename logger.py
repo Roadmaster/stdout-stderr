c1100d1315410c8413b304008b628c1bf38e7cbb
Daniel Manrique <daniel.manrique@fly.io>
2024-03-26 12:41:11 -0400

    things should work

--- logger.py
+++ logger.py
@@ -4,5 +4,5 @@ import sys
 while True:
     time.sleep(2)
     ti = time.time()
-    print(f"This is stdout, {ti}")
+    print(f"This is stdout, {ti}", file=sys.stdout)
     print(f"This is stderr, {ti}", file=sys.stderr)
